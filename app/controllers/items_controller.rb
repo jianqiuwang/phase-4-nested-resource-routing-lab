0bc3127e1197cf216e056e0fbe85bda8760fac4c
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-09 12:46:28 -0600

    commit tree 2f63316740457e5154693d80eaec3680e877e70a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  # POST /users/:user_id/items
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      new_item = user.items.create(item_params)
+      render json: new_item, status: :created
+    end
+  end
+
+private
+
+def render_not_found_response
+  render json: { error: "not found" }, status: :not_found
+end
+
+def item_params
+  params.permit(:name, :description, :price)
+end
 
 end
